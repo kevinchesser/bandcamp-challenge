711297e70473ea5332ee0d3731a802df5029d6cc
kevinchesser <kevin.chesser94@gmail.com>
2017-02-08 22:04:26 -0500

    add script to combine the files

---
+++ warm/combine.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cat Bandcamp\ -\ You’re\ Getting\ Warm\ -\ part51.txt Bandcamp\ -\ You’re\ Getting\ Warm\ -\ part92.txt Bandcamp\ -\ You’re\ Getting\ Warm\ -\ part14.txt Bandcamp\ -\ You’re\ Getting\ Warm\ -\ part71.txt Bandcamp\ -\ You’re\ Getting\ Warm\ -\ part60.txt Bandcamp\ -\ You’re\ Getting\ Warm\ -\ part20.txt Bandcamp\ -\ You’re\ Getting\ Warm\ -\ part82.txt > completewav.wav
